505801029381e9a4b8053964d2750783205a6f7b
Srijan Basnet <basnetsrijan202@gmail.com>
2026-07-15 17:57:53 +0545

    Add arithmetic sequence program

---
+++ DS/Assisment/First Lab/fifth.py
@@ -0,0 +1,9 @@
+# Program to find the nth term of an arithmetic sequence
+
+first_term = 3
+difference = 4
+n = 10
+
+nth_term = first_term + (n - 1) * difference
+
+print("The 10th term of the arithmetic sequence is:", nth_term)
