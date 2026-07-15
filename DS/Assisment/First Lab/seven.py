fb404290d8e7aa5ed59c992a7de49a22bcea9080
Srijan Basnet <basnetsrijan202@gmail.com>
2026-07-15 17:58:55 +0545

    Add infinite geometric series program

---
+++ DS/Assisment/First Lab/seven.py
@@ -0,0 +1,8 @@
+# Program to find the sum of an infinite geometric series
+
+first_term = 1
+common_ratio = 1 / 3
+
+sum_of_series = first_term / (1 - common_ratio)
+
+print("The sum of the infinite geometric series is:", sum_of_series)
